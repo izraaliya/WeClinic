92328f09a8afc3cd76773dca638e5f67ab89f73d
Noorzahara <97963503+Noorzahara@users.noreply.github.com>
2025-05-18 23:54:06 +0700

    Update: deskripsi perubahan yang kamu lakukan

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import { useEffect } from 'react';
 import Navbar from './components/navbar';
 import Home from './pages/home';
 import About from './pages/about';
 import PilihanDokter from './pages/doctor';
 import ReservasiDokter from './pages/reservasi';
 
+// Tambahkan fungsi ScrollToTop langsung di sini
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
+
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
@@ -18,6 +31,5 @@ function App() {
     </Router>
   );
 }
-
 
 export default App;
